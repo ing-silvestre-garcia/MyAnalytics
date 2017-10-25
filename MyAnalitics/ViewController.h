31fa89a357771bb7e9d1f9e6ab6b1b4778f5390b
Silvestre Garcia <silver@Silvestres-iMac.local>
2017-10-25 18:26:07 -0500

    Initial Commit

---
+++ MyAnalitics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyAnalitics
+//
+//  Created by Silvestre Garcia on 10/25/17.
+//  Copyright © 2017 silver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyAnalitics/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyAnalitics
+//
+//  Created by Silvestre Garcia on 10/25/17.
+//  Copyright © 2017 silver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
