0c8887cd78df12f6e2bb07134d426d2f5c469f59
Silvestre Garcia <silver@Silvestres-iMac.local>
2017-10-25 18:56:35 -0500

    Archivos nuevo proyecto Se agrega configuraron con google analytics de firebase

---
+++ Pods/Target Support Files/Pods-MyAnalitics/Pods-MyAnalitics-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyAnalitics : NSObject
+@end
+@implementation PodsDummy_Pods_MyAnalitics
+@end
---
+++ Pods/Target Support Files/Pods-MyAnaliticsTests/Pods-MyAnaliticsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyAnaliticsTests : NSObject
+@end
+@implementation PodsDummy_Pods_MyAnaliticsTests
+@end
