61c09d8bbb50da6de3803b83da1e842fa1acd1fc
Silvestre Garcia <silver@Silvestres-iMac.local>
2017-10-25 20:44:32 -0500

    Se completa código con lo de la clase

--- MyAnalitics/Home.h
+++ MyAnalitics/Home.h
@@ -5,11 +5,13 @@
 //  Created by Silvestre Garcia on 10/25/17.
 //  Copyright © 2017 silver. All rights reserved.
 //
-
 #import <UIKit/UIKit.h>
 
-@interface Home : UIViewController
+@import Firebase;
+@import GoogleSignIn;
 
+@interface Home : UIViewController<GIDSignInUIDelegate>
+- (IBAction)btnSignInPressed:(id)sender;
 
 @end
 
